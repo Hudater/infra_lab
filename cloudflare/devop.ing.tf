43bb964cd95e34c966add7f5c24938ada4756b9c
Harshit Mani Tripathi <harshit@hudater.dev>
2025-11-06 00:34:09 +0530

    rename CF CI Job and revert devop.ing cname test

--- cloudflare/devop.ing.tf
+++ cloudflare/devop.ing.tf
@@ -6,12 +6,3 @@ resource "cloudflare_dns_record" "alias_test" {
   zone_id  = var.zone_id_devop_ing
   settings = {}
 }
-
-resource "cloudflare_dns_record" "test_ci" {
-  content  = "1.1.1.1"
-  name     = "pr.devop.ing"
-  ttl      = 1
-  type     = "A"
-  zone_id  = var.zone_id_devop_ing
-  settings = {}
-}
