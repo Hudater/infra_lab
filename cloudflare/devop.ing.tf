af853cbf0325c5d3ffc82c91355f13eaff3db015
Harshit Mani Tripathi <harshit@hudater.dev>
2025-11-09 02:38:24 +0530

    temp records for devoping

--- cloudflare/devop.ing.tf
+++ cloudflare/devop.ing.tf
@@ -6,3 +6,22 @@ resource "cloudflare_dns_record" "alias_test" {
   zone_id  = var.zone_id_devop_ing
   settings = {}
 }
+resource "cloudflare_dns_record" "OCI_ZRH_ARM_devop_ing_traefik" {
+  content  = var.oci-zrh-arm-ip
+  name     = "devop.ing"
+  proxied  = false
+  ttl      = 1
+  type     = "A"
+  zone_id  = var.zone_id_devop_ing
+  settings = {}
+}
+
+resource "cloudflare_dns_record" "OCI_ZRH_ARM_devop_ing_wildcard_traefik" {
+  content  = "devop.ing"
+  name     = "*.devop.ing"
+  proxied  = false
+  ttl      = 1
+  type     = "CNAME"
+  zone_id  = var.zone_id_devop_ing
+  settings = {}
+}
