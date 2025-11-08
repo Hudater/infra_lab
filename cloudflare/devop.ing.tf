eec24d92457454db59e1c623468064132f0e8203
Harshit Mani Tripathi <harshit@hudater.dev>
2025-11-09 02:42:18 +0530

    removed conflicting record

--- cloudflare/devop.ing.tf
+++ cloudflare/devop.ing.tf
@@ -1,11 +1,3 @@
-resource "cloudflare_dns_record" "alias_test" {
-  content  = "1.1.1.1"
-  name     = "devop.ing"
-  ttl      = 1
-  type     = "A"
-  zone_id  = var.zone_id_devop_ing
-  settings = {}
-}
 resource "cloudflare_dns_record" "OCI_ZRH_ARM_devop_ing_traefik" {
   content  = var.oci-zrh-arm-ip
   name     = "devop.ing"
