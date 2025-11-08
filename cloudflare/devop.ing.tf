6e48b2844f99feb72c0db2b8629ce7cd688fadec
Harshit Mani Tripathi <harshit@hudater.dev>
2025-11-09 02:47:01 +0530

    revert to old dns for devoping

--- cloudflare/devop.ing.tf
+++ cloudflare/devop.ing.tf
@@ -1,19 +1,8 @@
-resource "cloudflare_dns_record" "OCI_ZRH_ARM_devop_ing_traefik" {
-  content  = var.oci-zrh-arm-ip
+resource "cloudflare_dns_record" "alias_test" {
+  content  = "1.1.1.1"
   name     = "devop.ing"
-  proxied  = false
   ttl      = 1
   type     = "A"
-  zone_id  = var.zone_id_devop_ing
-  settings = {}
-}
-
-resource "cloudflare_dns_record" "OCI_ZRH_ARM_devop_ing_wildcard_traefik" {
-  content  = "devop.ing"
-  name     = "*.devop.ing"
-  proxied  = false
-  ttl      = 1
-  type     = "CNAME"
   zone_id  = var.zone_id_devop_ing
   settings = {}
 }
