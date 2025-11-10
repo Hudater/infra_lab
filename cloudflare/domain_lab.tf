90cc4dfeaecbb0e8595172efd9866edc3a9eb0cd
Harshit Mani Tripathi <harshit@hudater.dev>
2025-11-11 00:55:56 +0530

    temp setting homelab cloud srv to ZRH-ARM instance to see if issue persists across machines with same ip

--- cloudflare/domain_lab.tf
+++ cloudflare/domain_lab.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_dns_record" "OCI_BOM_ARM_domain_lab_traefik" {
-  content  = var.oci-bom-arm-ip
+  content  = var.oci-zrh-arm-ip
   name     = var.domain_lab
   proxied  = true
   ttl      = 1
