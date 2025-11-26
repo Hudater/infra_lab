bdee07ddcb23257f63fd7f7516966d9f14ba3566
Harshit Mani Tripathi <harshit@hudater.dev>
2025-11-26 16:02:04 +0530

    removed devop.ing var ref

---
+++ oci/variable.tf
@@ -0,0 +1,11 @@
+variable "ocid_eu_zrh" {
+  description = "OCID for ZRH region"
+  type        = string
+  sensitive   = true
+}
+
+variable "oci_zrh_region" {
+  description = "OCID for ZRH region"
+  type        = string
+  sensitive   = true
+}
