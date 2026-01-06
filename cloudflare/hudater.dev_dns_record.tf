1bc0391051bb7928ec178d6681cf353209936525
Harshit Mani Tripathi <harshit@hudater.dev>
2026-01-06 15:52:54 +0530

    adding test domain

--- cloudflare/hudater.dev_dns_record.tf
+++ cloudflare/hudater.dev_dns_record.tf
@@ -58,6 +58,16 @@ resource "cloudflare_dns_record" "blog_hudater_dev" {
   settings = {}
 }
 
+resource "cloudflare_dns_record" "test_hudater_dev" {
+  content  = "hudater.github.io"
+  name     = "test.hudater.dev"
+  proxied  = false
+  ttl      = 1
+  type     = "CNAME"
+  zone_id  = var.zone_id_hudater_dev
+  settings = {}
+}
+
 resource "cloudflare_dns_record" "hashnode_hudater_dev" {
   content  = "hashnode.network"
   name     = "hashnode.hudater.dev"
